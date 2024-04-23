b38380323ae4b65f73b1ee8d24831f82995e4118
김세현 <dnjfdid14@naver.com>
2024-04-23 16:45:10 +0900

    whisper

---
+++ text_recognition.py
@@ -0,0 +1,43 @@
+import whisper
+from datetime import timedelta
+from googletrans import Translator
+
+model = whisper.load_model('base')
+trans = Translator()
+
+def sec_to_hour_min_sec(seconds):
+    dt = timedelta(seconds=seconds)
+    hours, remainder = divmod(dt.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return '{}:{:02d}:{:02d}'.format(hours, minutes, seconds)
+
+
+async def async_text_recognition(path):
+    res = model.transcribe(path)
+    txt = []
+        
+    for seg in res['segments']:
+        basic_txt = seg['text']
+        # txt_to_trans = seg['text']
+        # if trans.detect(txt_to_trans).lang == 'ko':
+        #     print('[{0} ~ ] {1}'.format(sec_to_hour_min_sec(seg['start']), txt_to_trans))
+        #     continue
+        
+        # trans_text = trans.translate(seg['text'], dest='ko').text
+        
+        try:
+            # 중복 확인
+            if basic_txt == basic_txt2:
+                continue
+            else:
+                print('[{0} ~ ] {1}'.format(sec_to_hour_min_sec(seg['start']), 
+                                        basic_txt))
+        except:
+            print('[{0} ~ ] {1}'.format(sec_to_hour_min_sec(seg['start']), 
+                                        basic_txt))
+        basic_txt2 = basic_txt
+        txt.append(str('[{0} ~ ] {1}'.format(sec_to_hour_min_sec(seg['start']), 
+                                             basic_txt)))
+    
+    return txt
+
